5e9fdadcf269345c704fa01d58b20c773d29535f
Naseef Mashood <nosfeeferatu@gmail.com>
2022-12-05 12:19:58 +0530

    feat: translates input to shakespearean

---
+++ app.js
@@ -0,0 +1,17 @@
+var inputText = document.querySelector("#input");
+var btnTranslate = document.querySelector("#button");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json";
+
+function fetchingServerURL(input){
+    return serverURL + "?" + "text=" + input;
+}
+
+btnTranslate.addEventListener("click", function eventHandler(){
+    fetch(fetchingServerURL(inputText.value))
+        .then(response => response.json())
+        .then(translate => {
+            outputDiv.innerText = translate.contents.translated;
+        })
+})
